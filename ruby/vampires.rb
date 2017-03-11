121ff12f3e2baa6b97c82df05b6dd984dc074cf9
intell-gent <nsksir@gmail.com>
2017-03-10 23:00:17 -0800

    add new emplyee questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,23 @@
+puts "How many employees will we be processing today?"
+employee_count = gets.chomp.to_i
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?" 
+age = gets.chomp
+
+puts "What year were you born?"
+birth = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_choice = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+insurance_andswer = gets.chomp
+
+puts "Welcome: #{name} to werewolf Inc."
+puts "It looks like your #{age} years old."
+puts "You were born in: #{birth}."
+puts "you Would like to have some garlic bread."
+puts "you are also intrested in our insurance program."
