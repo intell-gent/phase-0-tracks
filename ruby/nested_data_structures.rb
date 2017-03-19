52429a0fcc2ea53de0eb1762bb54c0df91695240
intell-gent <nsksir@gmail.com>
2017-03-19 02:02:23 -0700

    added has to hash practic

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,18 @@
+highway = {
+	carpool_lane: { 
+		car1: ["Joe", "Moe", "Doe"],
+		car2: ["Ricky"], # ooooh he is in the carpool lane with no passengers
+		car3: ["Becky", "John", "Tonya", "George"]
+	},
+	fast_lane: { 
+		car: ["BatMan"], # I am Batman, going fast
+		suv: ["The Rock"], # Can you smell the rock burnning rubber
+		pickup: ["Bod the Builder"] # Driving fast to go fix it.
+	},
+	slow_lane: { 
+		truck: ["Cleatus, 'Long Haul', Masterson"], # Yee Haw
+		minivan: ["Uncle Ben", "Aunt May", "Peter"], # spiderman be nice to your uncle
+		car: [] # must be one of them new fangled robot cars by them their google folks
+	},
+	sholder: ["A Boken down Car"]
+}
