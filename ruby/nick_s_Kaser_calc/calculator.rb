68af534c496ae35e56aec4a4fb09d7ff1cb29fd4
intell-gent <nsksir@gmail.com>
2017-03-20 01:23:56 -0700

    add user interface user_calc method

--- ruby/nick_s_Kaser_calc/calculator.rb
+++ ruby/nick_s_Kaser_calc/calculator.rb
@@ -1,3 +1,18 @@
+# create a user interface
+def user_calc
+  puts "welcome to your calculator."
+	loop do 
+	puts "what shal i calculate?"
+	answer = gets.chomp
+	# splits the answer
+	break if answer.downcase == "done"
+	calculations.push(answer)
+	answer = answer.split(" ")
+	result = calculate(answer[0].to_f, answer[1], answer[2].to_f)
+	p result
+	end
+end
+
 # Write a calculate method
 def calculate(integer, operator, integer2)
 	if operator == "+"
