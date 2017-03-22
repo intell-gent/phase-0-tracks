c9fdb7b824ed71938371d710d143e29031b28f49
intell-gent <nsksir@gmail.com>
2017-03-21 17:50:09 -0700

    add test comments from irb

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,4 +1,27 @@
 # class of puppy
+class puppy
 # fur color
 # weight
 # age
+end
+
+# Tests in IRB
+# Puppy.methods
+# duchess = Puppy.new
+# fido = Puppy.new
+# spot = Puppy.new
+
+# spot.class
+# duchess == fido => false
+# fido.instance_of?(Array) => false
+# fido.instance_of?(Puppy) => true
+# spot.play_dead => does not exist
+
+# str = String.new
+# str.length => 0
+# str.empty? => true
+# str += "huh."
+# str = "some string"
+# greeting = String.new("hello")
+# greeting.length => 5
+# greeting.upcase => "HELLO"
