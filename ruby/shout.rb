87b14376925e102a5d0d49a62ba4bda249810418
intell-gent <nsksir@gmail.com>
2017-03-26 05:44:21 -0700

    add angry method and happy method

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,11 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  def self.yell_happily(words)
+    words.upcase + "!" + " :)"
+  end
+  # Driver Code
+  yell_happily("hello")
+  yell_angrily("good buy")
 end
