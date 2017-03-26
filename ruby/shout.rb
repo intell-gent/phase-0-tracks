1096d5110a4abf13cf701a74f2add64513d1cc4e
intell-gent <nsksir@gmail.com>
2017-03-26 06:07:52 -0700

    add new shout module and Angry, Happy class

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,31 @@
+# old code
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yell_happily(words)
+#     words.upcase + "!" + " :)"
+#   end
+#   # Driver Code
+#   yell_happily("hello")
+#   yell_angrily("good buy")
+# end
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yell_happily(words)
-    words.upcase + "!" + " :)"
-  end
-  # Driver Code
-  yell_happily("hello")
-  yell_angrily("good buy")
+	def yell(words)	
+		words + "!!!"
+	end
 end
+
+class Angry
+	include Shout
+end
+
+class Happy
+	include Shout
+end
+# driver code
+angry = Angry.new
+p angry.yell("hello :(".upcase)
+
+happy = Happy.new
+p happy.yell("hello :)")
