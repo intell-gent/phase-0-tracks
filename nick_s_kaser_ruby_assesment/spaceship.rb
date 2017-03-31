d3ae03dfab49cea8bb4aaf6fa7c7263e7226e973
intell-gent <nsksir@gmail.com>
2017-03-30 18:59:54 -0700

    add accessor and reader to code and test with driver code

--- nick_s_kaser_ruby_assesment/spaceship.rb
+++ nick_s_kaser_ruby_assesment/spaceship.rb
@@ -1,4 +1,6 @@
 class Spaceship
+  attr_accessor :name
+  attr_reader :speed
   def initialize(name, speed)
     @name = name
     @speed = speed
@@ -10,3 +12,10 @@ ship1 = Spaceship.new( "USS Enterprise", 200000)
 p ship1
 ship2 = Spaceship.new( "HMS Andromeda", 108277)
 p ship2
+
+p ship1.name
+ship1.name = "SSS S S S"
+p ship1.name
+p ship1.speed
+# ship1.speed = 2000 works as intended by throwing and error
+p ship1
