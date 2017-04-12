f6c0fa3a7096168504b24a9a94c1b7df2a0c33f9
intell-gent <nsksir@gmail.com>
2017-04-12 11:30:11 -0700

    add sctripts to script file

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,8 @@
+var photo = document.getElementById('doge');
+photo.style.border = "3px dashed green";
+
+var headers = document.getElementsByClassName('headers');
+headers[0].style.fontSize = '20px';
+
+var paragraph = document.getElementsByTagName('p');
+paragraph[0].style.fontWeight = "bold";
