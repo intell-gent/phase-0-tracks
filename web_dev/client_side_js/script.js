4b22e55d7f5e2f49b3a135babb07cc150de03426
intell-gent <nsksir@gmail.com>
2017-04-12 11:52:12 -0700

    add event listeners to script file

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,8 +1,33 @@
+// ---------------RELEASE 1-------------------------
+// var photo = document.getElementById('doge');
+// photo.style.border = "3px dashed green";
+// var headers = document.getElementsByClassName('headers');
+// headers[0].style.fontSize = '20px';
+// var paragraph = document.getElementsByTagName('p');
+// paragraph[0].style.fontWeight = "bold";
+// ---------------RELEASE 2-------------------------
+function addGreenDashedBorder () {
+  event.target.style.border = "3px dashed green";
+};
 var photo = document.getElementById('doge');
-photo.style.border = "3px dashed green";
-
+photo.addEventListener("click", addGreenDashedBorder);
+function makeTextBig () {
+  event.target.style.fontSize = '40px';
+};
 var headers = document.getElementsByClassName('headers');
-headers[0].style.fontSize = '20px';
-
+headers[0].addEventListener("mouseover", makeTextBig);
+function makeTextsmall () {
+  event.target.style.fontSize = '20px';
+};
+var headers = document.getElementsByClassName('headers');
+headers[0].addEventListener("mouseout", makeTextsmall);
+function makeTextBold () {
+  event.target.style.fontWeight = "bold";
+}
 var paragraph = document.getElementsByTagName('p');
-paragraph[0].style.fontWeight = "bold";
+paragraph[0].addEventListener("mousedown", makeTextBold);
+function makeTextNotBold () {
+  event.target.style.fontWeight = "";
+}
+var paragraph = document.getElementsByTagName('p');
+paragraph[0].addEventListener("mouseup", makeTextNotBold);
