1f0c73b32252f8c4cb50341e5837fcbdecce6090
intell-gent <nsksir@gmail.com>
2017-04-13 15:11:31 -0700

    fixed jqry refrence in index, modified style file and add background and focus class and add jqury commands

--- web_dev/jquery/jqry.js
+++ web_dev/jquery/jqry.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-    $('.para').hide();
+    $('#ttp').on("mouseenter", function(){
+      $(this).addClass("focus");
+    });
+    $('#ttp').on("mouseleave", function(){
+      $(this).removeClass("focus");
+    });
+    $('.list').on("click", function(){
+      var answer = prompt("Add a reason to trust the process.");
+      $(this).append("<li>" +answer+ "</li>");
+    });
+     $("#trust_button").on("click", function(){
+      $(this).replaceWith( "<audio id='Yes' controls> <source src='Yes.mp3' type='audio/mp3'></audio>" );
+    });
 });
-$('.para').hide();
