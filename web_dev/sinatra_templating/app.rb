c0d7b92dcd94948bf908674716ddec84d47159a7
intell-gent <nsksir@gmail.com>
2017-04-14 13:05:04 -0700

    project compleat

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'sinatra'
+require "sinatra/reloader"
 require 'sqlite3'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
@@ -20,8 +21,17 @@ end
 # create new students via
 # a form
 post '/students' do
-  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [ params['name'], params['campus'], params['age'].to_i ])
   redirect '/'
 end
 
 # add static resources
+get '/students/update' do
+  erb :update_student
+end
+
+post '/students/update' do
+  p params
+  db.execute("UPDATE students SET name=?, age=?, campus=? WHERE id=?", [ params['name'], params['age'].to_i, params['campus'], params['id'].to_i ])
+  redirect '/'
+end
