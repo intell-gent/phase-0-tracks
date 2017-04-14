1decb61dd25aa4cb45a0523394c98562a56fd0f6
intell-gent <nsksir@gmail.com>
2017-04-14 01:10:33 -0700

    add functions

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,11 +1,12 @@
 # require gems
 require 'sinatra'
+require "sinatra/reloader"
 require 'sqlite3'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
-# write a basic GET route
+# write a basic GET routen 
 # add a query parameter
 # GET /
 get '/' do
@@ -20,6 +21,7 @@ get '/about/:person' do
 end
 
 get '/:person_1/loves/:person_2' do
+
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
@@ -44,3 +46,55 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a get /contact route that displays an address
+get '/address' do
+  "<h3>633 Folsom, <br> San Francisco,<br> CA 94107 </h3>"
+end
+# A /great_job route that can take a person's name as a query parameter
+# say "Good job, [person's name]!". 
+# ELSE, the route simply says "Good job!"
+get '/great_job/:person' do
+  if params[:person]
+    "Good job, #{params[:person]}!"
+  else
+    "Good job!"
+  end
+end
+# A route that uses route parameters to add two numbers and respond with the result.
+get '/add/:num1/:num2' do 
+  total = params[:num1].to_i + params[:num2].to_i
+  "#{params[:num1]} + #{params[:num2]} = #{total}"
+end
+
+# Optional bonus: Make a route that allows the user 
+# to search the database in some way 
+get '/search/:name' do
+  students = db.execute("SELECT * FROM students")
+  response = ""
+  students.each do |student|
+    current_name = "#{student['name']}".split(" ")
+    if current_name[0] == "Mr." || current_name[0] == "Ms." || current_name[0] == "Mrs." || current_name[0] == "Dr." || current_name[0] == "Miss" 
+      if current_name[1]  == params[:name]
+        response << "ID: #{student['id']}<br>"
+        response << "Name: #{student['name']}<br>"
+        response << "Age: #{student['age']}<br>"
+        response << "Campus: #{student['campus']}<br><br>"
+      end
+    else
+        if current_name[0]  == params[:name]
+          response << "ID: #{student['id']}<br>"
+          response << "Name: #{student['name']}<br>"
+          response << "Age: #{student['age']}<br>"
+          response << "Campus: #{student['campus']}<br><br>"
+        end
+    end 
+  end
+  response
+end
+
+# checks output
+# get '/input/:output' do
+#   output = params[:output]
+#   "#{output} is a #{output.class}."
+#  end
